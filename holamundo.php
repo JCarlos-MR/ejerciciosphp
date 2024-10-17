e76d6886adca91565dc55e395006d0f4d2066389
Carlos Mejia <mejiarodriguez23@iespabloserrano.com>
2024-10-17 20:47:09 +0200

    asdf

--- holamundo.php
+++ holamundo.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initiallll-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <?php
     //Tú código PHP aquí
-    echo "Jean Carlos Mejia Rodriguez";
+    echo "Jean Carlos Mejia Rodriguez desde Visusldgal Studio Code";
     ?>
 </body>
 </html>
